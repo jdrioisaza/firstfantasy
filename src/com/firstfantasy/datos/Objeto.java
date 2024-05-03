4e4ca55ddfbc2d41bd80449f51074ee5266adc8d
diomedescerda <kenmatwofour@gmail.com>
2024-05-03 11:45:10 -0500

    prueba

--- src/com/firstfantasy/datos/Objeto.java
+++ src/com/firstfantasy/datos/Objeto.java
@@ -1,5 +1,19 @@
 package com.firstfantasy.datos;
 
 public class Objeto {
+    int id;
+    String nombre;
     
+    public void obtener(){
+        
+    }
+    
+    
+    public void usar(){
+        
+    }
+    
+    public String mostrarDetalles(){
+        return "";
+    }
 }
