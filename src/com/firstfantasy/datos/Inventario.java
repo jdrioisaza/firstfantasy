95439468de87178a2fe8452f7b17dce1a186e164
Fleshmat <gadiel.comas2015@gmail.com>
2024-05-03 12:41:24 -0500

    1_inventario

--- src/com/firstfantasy/datos/Inventario.java
+++ src/com/firstfantasy/datos/Inventario.java
@@ -4,12 +4,33 @@ import java.util.ArrayList;
 
 public class Inventario {
     
-    ArrayList<Arma> armas;
-    ArrayList<Pocion> pociones;
+    private ArrayList<Arma> armas;
+    private ArrayList<Pocion> pociones;
 
     public Inventario(ArrayList<Arma> armas, ArrayList<Pocion> pociones) {
         this.armas = armas;
         this.pociones = pociones;
     }
     
+    public void addArma(Arma arma){
+        armas.add(arma);
+    }
+    
+    public void addPocion(Pocion pocion){
+        pociones.add(pocion);
+    }
+
+    /**
+     * @return the armas
+     */
+    public ArrayList<Arma> getArmas() {
+        return armas;
+    }
+
+    /**
+     * @return the pociones
+     */
+    public ArrayList<Pocion> getPociones() {
+        return pociones;
+    }
 }
