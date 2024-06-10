2cc2f761f28bd7dc6b763c7df7387b5755ae6fe8
Mauricio Polo <mauriciopoloem@gmail.com>
2024-06-10 11:55:00 -0500

    Update Enemigo.java and Heroe.java

--- src/com/firstfantasy/personajes/Heroe.java
+++ src/com/firstfantasy/personajes/Heroe.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 
 public class Heroe extends Personaje{
 
-    public Heroe(int identificador, String nombre, int vida, int energia, int atributoAtaque, int atributoAtaqueEspecial, int atributoDefensa, int atributoDefensaEspecial, Raza raza, Clase clase, Arma armaEquipada, ArrayList<Habilidad> habilidades, ArrayList<Arma> armas, ArrayList<Pocion> pociones) {
-        super(identificador, nombre, vida, energia, atributoAtaque, atributoAtaqueEspecial, atributoDefensa, atributoDefensaEspecial, raza, clase, armaEquipada, habilidades, armas, pociones);
+    public Heroe(int identificador, String nombre, int vida, int energia, int atributoAtaque, int atributoAtaqueEspecial, int atributoDefensa, int atributoDefensaEspecial, Raza raza, Clase clase, Arma armaEquipada, ArrayList<Habilidad> habilidades, ArrayList<Arma> armas, ArrayList<Pocion> pociones, String tipo) {
+        super(identificador, nombre, vida, energia, atributoAtaque, atributoAtaqueEspecial, atributoDefensa, atributoDefensaEspecial, raza, clase, armaEquipada, habilidades, armas, pociones, tipo);
     }
     
 }
